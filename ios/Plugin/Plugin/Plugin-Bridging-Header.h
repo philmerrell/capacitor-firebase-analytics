2fe7bfe3b923351f8e2483ee081e82673482bc47
Phil Merrell <philmerrell@boisestate.edu>
2019-01-12 14:06:49 -0700

    ios: Add Bridging Header for Firebase

---
+++ ios/Plugin/Plugin/Plugin-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Plugin-Bridging-Header.h
+//  Plugin
+//
+//  Created by Phil Merrell on 1/12/19.
+//  Copyright © 2019 Max Lynch. All rights reserved.
+//
+
+#ifndef Plugin_Bridging_Header_h
+#define Plugin_Bridging_Header_h
+
+#import <Firebase/Firebase.h>
+
+#endif /* Plugin_Bridging_Header_h */
