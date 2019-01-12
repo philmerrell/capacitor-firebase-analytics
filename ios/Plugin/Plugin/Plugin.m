f2b92e2baef5156040e7d6cf912dbb92663a57a7
Phil Merrell <philmerrell@boisestate.edu>
2019-01-12 14:51:51 -0700

    ios: Expose methods to Capacitor

--- ios/Plugin/Plugin/Plugin.m
+++ ios/Plugin/Plugin/Plugin.m
@@ -4,5 +4,10 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(CapacitorFirebaseAnalytics, "CapacitorFirebaseAnalytics",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(logEvent, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(setUserProperty, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(setUserId, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(setScreenName, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(appInstanceId, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(resetAnalyticsData, CAPPluginReturnNone);
 )
