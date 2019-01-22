e5e81d55b093769ffc445d1a659cf7c0ff9c5d17
Phil Merrell <philmerrell@boisestate.edu>
2019-01-21 21:32:23 -0700

    fix: setScreen optional parameter

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,6 +1,6 @@
 declare global {
   interface PluginRegistry {
-    CapacitorFirebaseAnalytics?: CapacitorFirebaseAnalyticsPlugin;
+    FirebaseAnalytics?: CapacitorFirebaseAnalyticsPlugin;
   }
 }
 
@@ -8,7 +8,7 @@ export interface CapacitorFirebaseAnalyticsPlugin {
   logEvent(options: { name: string, parameters: any[] }): void;
   setUserProperty(options: { value: string, name: string }): void;
   setUserId(options: { userId: string }): void;
-  setScreenName(options: { screenName: string, screenClassOverride: string }): void;
+  setScreenName(options: { screenName: string, screenClassOverride?: string }): void;
   appInstanceId(): string;
   resetAnalyticsData(): void;
 }
