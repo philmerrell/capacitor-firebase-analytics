eacffe132c18beb336ba2a2422b6d854e4fa7f01
Phil Merrell <philmerrell@boisestate.edu>
2019-01-28 23:00:35 -0700

    update logEvent interface

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,11 +1,12 @@
 declare global {
   interface PluginRegistry {
-    CapacitorFirebaseAnalytics?: CapacitorFirebaseAnalyticsPlugin;
+    FirebaseAnalytics?: CapacitorFirebaseAnalyticsPlugin;
   }
 }
 
 export interface CapacitorFirebaseAnalyticsPlugin {
-  logEvent(options: { name: string, parameters: any[] }): void;
+  
+  logEvent(options: { name: string, parameters: object }): void;
   setUserProperty(options: { value: string, name: string }): void;
   setUserId(options: { userId: string }): void;
   setScreenName(options: { screenName: string, screenClassOverride: string }): void;
