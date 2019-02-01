95c171d82285fb8502e1534e7c7500828b11de3c
James Brooks <jamesbrooks1@boisestate.edu>
2019-02-01 09:40:11 -0700

    add exception handling and error messaging

--- android/capacitor-firebase-analytics/src/main/java/com/philmerrell/firebaseanalytics/CapacitorFirebaseAnalytics.java
+++ android/capacitor-firebase-analytics/src/main/java/com/philmerrell/firebaseanalytics/CapacitorFirebaseAnalytics.java
@@ -21,88 +21,117 @@ public class CapacitorFirebaseAnalytics extends Plugin {
     private FirebaseAnalytics firebaseAnalytics;
     private static final String TAG = "CapacitorFirebaseAnalyticsPlugin";
 
-    public void load(){
+    public void load() {
         firebaseAnalytics = FirebaseAnalytics.getInstance(getContext());
     }
 
     @PluginMethod()
     public void logEvent(PluginCall call) throws JSONException {
-        final String name = call.getString("name", null);
-        JSObject data = call.getData();
-        final JSONObject params =  data.optJSONObject("parameters");
-        if(name != null){
-            Bundle bundle = new Bundle();
-
-            if(params != null) {
-                Iterator<String> keys = params.keys();
-
-                while (keys.hasNext()) {
-                    String key = keys.next();
-                    Object value = params.get(key);
-
-                    if (value instanceof String) {
-                        bundle.putString(key, (String) value);
-                    } else if (value instanceof Integer) {
-                        bundle.putInt(key, (Integer) value);
-                    } else if (value instanceof Double) {
-                        bundle.putDouble(key, (Double) value);
-                    } else if (value instanceof Long) {
-                        bundle.putLong(key, (Long) value);
-                    } else {
-                        Log.w(TAG, "Value for key " + key + " not one of (String, Integer, Double, Long)");
-                    }
-                }
+        try {
+            final String name = call.getString("name");
+            final String value = call.getString("value");
+            if (name != null && value != null) {
+                firebaseAnalytics.setUserProperty(name, value);
+                call.success();
+            } else {
+                call.reject("key 'name' or 'value' does not exist");
             }
-            firebaseAnalytics.logEvent(name, bundle);
+        } catch (Exception e) {
+            call.reject(e.getLocalizedMessage(), e);
         }
     }
 
     @PluginMethod()
     public void setUserProperty(PluginCall call) {
-        final String name = call.getString("name");
-        final String value = call.getString("value");
+        try {
+            final String name = call.getString("name", null);
+            JSObject data = call.getData();
+            final JSONObject params = data.optJSONObject("parameters");
+            if (name != null) {
+                Bundle bundle = new Bundle();
 
-        if(name != null && value !=null){
-            firebaseAnalytics.setUserProperty(name, value);
+                if (params != null) {
+                    Iterator<String> keys = params.keys();
+
+                    while (keys.hasNext()) {
+                        String key = keys.next();
+                        Object value = params.get(key);
+
+                        if (value instanceof String) {
+                            bundle.putString(key, (String) value);
+                        } else if (value instanceof Integer) {
+                            bundle.putInt(key, (Integer) value);
+                        } else if (value instanceof Double) {
+                            bundle.putDouble(key, (Double) value);
+                        } else if (value instanceof Long) {
+                            bundle.putLong(key, (Long) value);
+                        } else {
+                            Log.w(TAG, "Value for key " + key + " not one of (String, Integer, Double, Long)");
+                        }
+                    }
+                } else {
+                    call.reject("key 'parameters' does not exist");
+                }
+                firebaseAnalytics.logEvent(name, bundle);
+                call.success();
+            } else {
+                call.reject("key 'name' does not exist");
+            }
+        } catch (Exception e) {
+            call.reject(e.getLocalizedMessage(), e);
         }
     }
 
     @PluginMethod()
     public void setUserId(PluginCall call) {
-        final String userId = call.getString("userId");
-
-        if(userId != null){
-            firebaseAnalytics.setUserId(userId);
+        try {
+            final String userId = call.getString("userId");
+            if (userId != null) {
+                firebaseAnalytics.setUserId(userId);
+                call.success();
+            } else {
+                call.reject("key 'userId' does not exist");
+            }
+        } catch (Exception e) {
+            call.reject(e.getLocalizedMessage(), e);
         }
     }
 
     @PluginMethod()
-    public void setScreenName(PluginCall call){
-        final String value = call.getString("screenName");
-        getActivity().runOnUiThread(new Runnable(){
-            @Override
-            public void run(){
-                firebaseAnalytics.setCurrentScreen(getActivity(), value, null);
-            }
-        });
+    public void setScreenName(PluginCall call) {
+        try {
+            final String value = call.getString("screenName");
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    firebaseAnalytics.setCurrentScreen(getActivity(), value, null);
+                    call.success();
+                }
+            });
+        } catch (Exception e) {
+            call.reject(e.getLocalizedMessage(), e);
+        }
     }
 
     @PluginMethod()
-    public String appInstanceId(){
-        return firebaseAnalytics.getAppInstanceId().toString();
+    public String appInstanceId(PluginCall call) {
+        try {
+            String appId = firebaseAnalytics.getAppInstanceId().toString();
+            JSObject ret = new JSObject();
+            ret.put("appId", appId);
+            call.success(ret);
+        } catch (Exception e) {
+            call.reject(e.getLocalizedMessage(), e);
+        }
     }
 
     @PluginMethod()
-    public void  resetAnalyticsData(){
-        firebaseAnalytics.resetAnalyticsData();
-    }
-
-    @PluginMethod()
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+    public void resetAnalyticsData(PluginCall call) {
+        try {
+            firebaseAnalytics.resetAnalyticsData();
+            call.success();
+        } catch (Exception e) {
+            call.reject(e.getLocalizedMessage(), e);
+        }
     }
 }
