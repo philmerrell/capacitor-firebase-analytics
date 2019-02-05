4eb9b7226471f11e9b4d72dd597dbd814c6efb69
Phil Merrell <philmerrell@boisestate.edu>
2019-02-05 11:48:53 -0700

    ios: Create Bridging Header file

--- ios/Plugin/Plugin/Plugin-Bridging-Header.h
+++ ios/Plugin/Plugin/Plugin-Bridging-Header.h
@@ -2,13 +2,13 @@
 //  Plugin-Bridging-Header.h
 //  Plugin
 //
-//  Created by Phil Merrell on 1/12/19.
+//  Created by Phil Merrell on 2/5/19.
 //  Copyright © 2019 Max Lynch. All rights reserved.
 //
 
 #ifndef Plugin_Bridging_Header_h
 #define Plugin_Bridging_Header_h
 
-#import "FirebaseAnalyticsPlugin.h"
+#import "Firebase.h"
 
 #endif /* Plugin_Bridging_Header_h */
